7c058ceada1bc3fc90859a1672c8a1928a568e08
Reno ROY <seenaroy81@gmail.com>
2025-10-14 01:15:09 +0530

    Fixed order controller bugs

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ await connectCloudinary();
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://greatstack-frontend-iota.vercel.app",
+  "https://greenreno.vercel.app",
 ];
 
 app.post("/stripe", express.raw({ type: "application/json" }), stripeWebhooks);
