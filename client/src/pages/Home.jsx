79a2bbdab74928dc222cde8711d20a39569425da
Reno ROY <seenaroy81@gmail.com>
2025-10-14 01:10:15 +0530

    Fixed order controller bugs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import MainBanner from "../components/MainBanner";
 import Categories from "../components/Categories";
 import BestSeller from "../components/BestSeller";
-import BottomBanner from "../components/Bottombanner";
+import BottomBanner from "../components/BottomBanner";
 import NewsLetter from "../components/NewsLetter";
 
 const Home = () => {
