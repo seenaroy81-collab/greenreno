3d9f74ce106caf94f563fb4d8b698b8bffdebc30
RENO ROY <seenaroy81@gmail.com>
2025-12-27 04:42:06 +0530

    Update userController.js

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -26,6 +26,7 @@ export const register = async (req, res) => {
       secure: process.env.NODE_ENV === "production", //use secure cookie in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF PROTECTION
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiration time
+      path: "/",
     });
     return res.json({
       success: true,
@@ -63,6 +64,7 @@ export const login = async (req, res) => {
       secure: process.env.NODE_ENV === "production", //use secure cookie in production
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict", //CSRF PROTECTION
       maxAge: 7 * 24 * 60 * 60 * 1000, //cookie expiration time
+      path: "/",
     });
     return res.json({
       success: true,
@@ -90,6 +92,7 @@ export const logout = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      path: "/",
     });
     return res.json({ success: true, message: "Logged Out" });
   } catch (error) {
